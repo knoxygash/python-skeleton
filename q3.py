65ab3a8ce266dfc30a0744c2f3a0543ef25263ee
knoxygash <romanasseh20@gmail.com>
2018-10-16 17:42:23 +0200

    Update q3.py

--- q3.py
+++ q3.py
@@ -4,5 +4,12 @@ import numpy as np
 
 def question03(numNodes, edgeList):
   # modify and then return the variable below
-  answer = -1
+  from sets import Set  
+  nodeElememts = Set([i[0] for i in edgeList])
+  edgeElements = Set([i[1] for i in edgeList])
+  
+  if nodeElememts.intersection(edgeElements)==Set([]):
+    answer = len(edgeElements)-len(nodeElememts)
+  else:
+    answer = len(edgeList)-len(nodeElememts.intersection(edgeElements))
   return answer
