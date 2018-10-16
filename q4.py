b184b591027b8738d72f0d427901a5cfdd5b9832
knoxygash <romanasseh20@gmail.com>
2018-10-16 17:43:26 +0200

    Update q4.py

--- q4.py
+++ q4.py
@@ -5,6 +5,14 @@ import numpy as np
 #compute all combinations for two portfolios
 def question04(rows, numberMachines):
   # modify and then return the variable below
-  answer = -1
+  lowest = -1
+  for row in rows:
+    rowLen = len(row)
+    for j in range(rowLen-numberMachines):
+      try:
+        lowest = sum(row[j:j+numberMachines])
+      except TypeError:
+        continue
+  answer = lowest
   return answer
 
