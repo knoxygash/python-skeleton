6571dc10ac4f94302606734a11668f01b008cc9f
knoxygash <romanasseh20@gmail.com>
2018-10-16 04:29:40 +0200

    Solution to Questions

--- q1.py
+++ q1.py
@@ -5,5 +5,12 @@ import numpy as np
 # modify this function, and create other functions below as you wish
 def question01(portfolios):
   # modify and then return the variable below
-  answer = 0
+  highestEval = 0
+  sizeOfports = len(portfolios)
+  for i in range(sizeOfports):
+    for j in range(i+1,sizeOfports):
+      eval = int(bin(portfolios[i] ^ portfolios[j]),2)
+      if eval > highestEval:
+        highestEval = eval    
+  answer = highestEval
   return answer
--- q2.py
+++ q2.py
@@ -5,6 +5,16 @@ import numpy as np
 #compute all combinations for two portfolios
 def question02(cashFlowIn, cashFlowOut):
   # modify and then return the variable below
-  answer = -1
+  from random import randint   
+  lowest = 1000
+  minSize = len(cashFlowIn) if len(cashFlowIn) < len(cashFlowOut) else len(cashFlowOut)
+  randomSetSize = randint(1,minSize) % 100
+  while lowest > 5:    
+    subIn = [cashFlowIn[randint(0,len(cashFlowIn)-1)] for i in range(randomSetSize)]
+    subOut = [cashFlowOut[randint(0,len(cashFlowOut)-1)] for i in range(randomSetSize)]
+    eval = abs(sum(subIn)-sum(subOut))
+    if eval < lowest:
+      lowest = eval
+  answer = lowest
   return answer
 
